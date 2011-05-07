602655cea3006dc31c910ab906c2f76b1f2f02b6
David Warner <david@iconiccity.com>
2011-05-07 00:22:18 -0700

    jQuery Cycle slideshows

--- site/js/script.js
+++ site/js/script.js
@@ -1,12 +1,42 @@
-$(function(){
+$(function() {
   $(".thumbs").each(function (t) {
-    var h = 0;
-    
-    $(this).children().each(function (c) {
-      var this_h = $(this).height();
-      h = (this_h > h) ? this_h : h;
-    });
-
-    $(this).height(h).scrollPane();
+    $(this)
+      .before("<nav class='pager'></nav>")
+      .cycle({
+        fx: 'scrollHorz',
+        speed:  'fast',
+        timeout: 0,
+        next: $(this).siblings('nav.next-prev').children('.next'),
+        prev: $(this).siblings('nav.next-prev').children('.prev'),
+        pager: $(this).siblings('nav.pager'),
+        pagerAnchorBuilder: function(idx, slide) { 
+          return '<a href="#">⚫</a>';
+        }
+      });
+      // .scrollpane();
   });
 });
+
+//   = link_to "⚫", "#"
+//   = link_to "⚪", "#"
+
+
+
+// $(function(){
+//   updateScroll();
+// });
+// 
+// function updateScroll() {
+//   $(".thumbs").each(function (t) {
+// 
+//     // Find the max height child for each pane
+//     var h = 0;
+//     $(this).children().each(function (c) {
+//       var this_h = $(this).height();
+//       h = (this_h > h) ? this_h : h;
+//     });
+// 
+//     // Set the height of each pane and turn on scrolling
+//     $(this).height(h).scrollpane();
+//   });
+// }
